5803ac9660ba8a4ebfebf8208d7209d158548f65
SOunit <workbookwork7@gmail.com>
2022-03-31 17:03:51 -0700

    separate files using graphql-tools/schema

---
+++ 17_graphql/orders/orders.model.js
@@ -0,0 +1,12 @@
+module.exports = [
+  {
+    date: "2005-05-03",
+    subtotal: 90.22,
+    items: [
+      {
+        product: { id: "redshoe", description: "old red shoe", price: 45.11 },
+        quantity: 2,
+      },
+    ],
+  },
+];
